17915768f8f3c8e45b4e23e265c14c2398067599
ArturoBarrios <35538894+ArturoBarrios@users.noreply.github.com>
2018-11-24 12:34:44 -0500

    Add files via upload

--- ClusteringIrisPlaces/plot_iris_dataset.py
+++ ClusteringIrisPlaces/plot_iris_dataset.py
@@ -29,34 +29,57 @@ from sklearn import datasets
 from sklearn.decomposition import PCA
 import loader
 
+
 x_labels = []
 f = open("p_xlabels.txt","r")
 line = f.read()
-
 for label in line.split(","):
     x_labels.append(label[1:len(label)-1])
+# x_labels = {"SepalLengthCm","SepalWidthCm","PetalLengthCm","PetalWidthCm"}
 X, y, type2id = loader.load_data('Iris_p.csv', y_label="Species", x_labels=x_labels)
 
+summation_X = []
+summation_Y = []
+for vector in X:
+    index = 1
+    result = 0
+    for v in vector:
+        if(v>0):
+            result+=(index*v)
+
+    index+=1
+    summation_X.append(result)
+
+print(summation_X)
+print(len(X[0]))
+
+#for i in y:
+    #for j in X[i]:
+        #print i," ",j
+    #print(X[:,i])
+# for x in y:
+#     print "y: ",x
+#     print "x: ", X[:,x]
 # import some data to play with
 # iris = datasets.load_iris()
-# print("IRIS: ",iris.data)
-# print("X: ",X)
-# # X = iris.data[:, :2]  # we only take the first two features.
-# print("X: ",X)
-# #y = iris.target
-# print("Y: ",y)
+# X = iris.data[:, :3]  # we only take the first two features.
+# y = iris.target
 
-x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-
+x_min, x_max = min(summation_X) , max(summation_X)
+y_min, y_max = y.min(), y.max()
+for i in y:
+    summation_Y.append((y_min+y_max)/2)
 plt.figure(2, figsize=(8, 6))
 plt.clf()
-
-# Plot the training points
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1,
+print(len(summation_X)," ",len(y))
+#Plot the training points
+plt.scatter(summation_X,summation_Y, c=y, cmap=plt.cm.Set1,
             edgecolor='k')
-plt.xlabel('Sepal length')
-plt.ylabel('Sepal width')
+print("max: ",x_max)
+print("min: ",x_min)
+
+plt.xlabel('Uncommon Word Count')
+plt.ylabel(' ')
 
 plt.xlim(x_min, x_max)
 plt.ylim(y_min, y_max)
@@ -67,9 +90,11 @@ plt.yticks(())
 # plot the first three PCA dimensions
 fig = plt.figure(1, figsize=(8, 6))
 ax = Axes3D(fig, elev=-150, azim=110)
-X_reduced = PCA(n_components=3).fit_transform(X)
-ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=y,
-           cmap=plt.cm.Set1, edgecolor='k', s=40)
+components = len(X[0])
+print(components)
+X_reduced = PCA(n_components=147).fit_transform(X)
+ax.scatter(summation_X,0, c=y,
+           cmap=plt.cm.Set1, edgecolor='k',s=40)
 ax.set_title("First three PCA directions")
 ax.set_xlabel("1st eigenvector")
 ax.w_xaxis.set_ticklabels([])
